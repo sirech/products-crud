bf3c8cd291a3fb88e7f98bd0bd053f0eb3d39b61
Mario Fernandez <mario.fernandez@hceris.com>
2020-12-05 13:44:40 +0100

    filter soft deleted items from queries

--- src/main/java/com/hceris/crud/products/ProductsRepository.java
+++ src/main/java/com/hceris/crud/products/ProductsRepository.java
@@ -1,6 +1,16 @@
 package com.hceris.crud.products;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ProductsRepository extends CrudRepository<Product, Long> {
+    @Override
+    @Query("select e from #{#entityName} e where e.id = ?1 and e.deleted = false")
+    Optional<Product> findById(Long aLong);
+
+    @Override
+    @Query("select e from #{#entityName} e where e.deleted = false")
+    Iterable<Product> findAll();
 }
--- src/test/java/com/hceris/crud/products/ProductsRepositoryTest.java
+++ src/test/java/com/hceris/crud/products/ProductsRepositoryTest.java
@@ -10,7 +10,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * This test is more of a sanity check to ensure that the setup is correctly done.
- *
+ * <p>
  * It doesn't really test any functionality implemented by us.
  */
 @DataJpaTest
@@ -20,8 +20,14 @@ class ProductsRepositoryTest {
     ProductsRepository repository;
 
     @Test
-    void findReturnsEmptyOptionalIfDoesntExist() {
+    void findReturnsEmptyIfDoesntExist() {
         Optional<Product> product = repository.findById(0L);
+        assertThat(product).isEmpty();
+    }
+
+    @Test
+    void findReturnsEmptyIfProductDeleted() {
+        Optional<Product> product = repository.findById(3L);
         assertThat(product).isEmpty();
     }
 
@@ -30,6 +36,14 @@ class ProductsRepositoryTest {
         Optional<Product> product = repository.findById(1L);
         assertThat(product).isNotEmpty();
         assertThat(product.get().getName()).isEqualTo("test_product");
+    }
+
+    @Test
+    void findAllExcludesDeletedItems() {
+        Iterable<Product> products = repository.findAll();
+        assertThat(products).allSatisfy(product -> {
+            assertThat(product.getDeleted()).isFalse();
+        });
     }
 
     @Test
