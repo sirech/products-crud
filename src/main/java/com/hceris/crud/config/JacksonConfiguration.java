9dd3ccc17a540d15b94c4d15cfc08b6502866750
Mario Fernandez <mario.fernandez@hceris.com>
2020-12-20 12:32:33 +0100

    Add Vavr

---
+++ src/main/java/com/hceris/crud/config/JacksonConfiguration.java
@@ -0,0 +1,19 @@
+package com.hceris.crud.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.vavr.jackson.datatype.VavrModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+@Configuration
+public class JacksonConfiguration {
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new VavrModule());
+        converter.setObjectMapper(objectMapper);
+        return converter;
+    }
+}
